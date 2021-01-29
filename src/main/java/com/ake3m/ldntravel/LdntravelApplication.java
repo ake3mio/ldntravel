ffbef5649cab956d9d87d53acb511093ac48868d
akeem faulkner <contact@ake3m.com>
2021-01-29 08:13:44 +0000

    initial commit

---
+++ src/main/java/com/ake3m/ldntravel/LdntravelApplication.java
@@ -0,0 +1,13 @@
+package com.ake3m.ldntravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdntravelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LdntravelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ake3m/ldntravel/LdntravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ake3m.ldntravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdntravelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
